7bbddbca302321521d3a9a705b132700936c13dd
Richard Abbenhuis <richard@abbenhuis.net>
2026-09-14 16:42:14 +0200

    Revert "Use hostname-derived From for report emails via send-mail helper"
    This reverts commit 69a37eedfb1496e3642ec5cf05d657648493aa6c.

--- ansible/roles/base/files/send-mail.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Send stdin to notify@abbenhuis.net with an envelope-from derived from this host,
-# so each container's mail is distinguishable (notify@<shortname>.abbenhuis.internal).
-exec msmtp -f "notify@$(hostname -s).abbenhuis.internal" notify@abbenhuis.net
